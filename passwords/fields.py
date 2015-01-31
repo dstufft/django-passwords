c106497c1b8cb8cea9d7c164f3b73cd3185f20ba
Sean Marlow <smarlowucf@gmail.com>
2015-01-31 11:26:58 -0700

    Quickfix
    reorder not in clause

--- passwords/fields.py
+++ passwords/fields.py
@@ -6,7 +6,7 @@ class PasswordField(CharField):
     default_validators = [validate_length, common_sequences, dictionary_words, complexity]
 
     def __init__(self, *args, **kwargs):
-        if not "widget" in kwargs:
+        if "widget" not in kwargs:
             kwargs["widget"] = PasswordInput(render_value=False)
 
         super(PasswordField, self).__init__(*args, **kwargs)
